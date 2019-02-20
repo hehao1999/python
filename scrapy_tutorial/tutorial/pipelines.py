64301d7d69209eefc0f6bd4cbd0a93d90e956f78
HeHao <1421771020@qq.com>
2019-02-20 13:51:08 +0800

    tutorial

---
+++ scrapy_tutorial/tutorial/pipelines.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
+import pymongo
+
+
+class TutorialPipeline(object):
+    # 筛选出TEXT长度大于50的数据
+    def __init__(self):
+        self.limit = 50
+
+    def process_item(self, item, spider):
+        if item['text']:
+            if len(item['text']) > self.limit:
+                item['text'] = item['text'][0:self.limit].rstrip() + '...'
+            return item
+        else:
+            return DropItem('Missing Text')  # 抛出异常，丢弃数据
+
+
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod # 装饰器装饰为类方法，与实例无关
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
+
+    def open_spider(self, spider):
+        # spider开启时方法被调用
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def process_item(self, item, spider):
+        name = item.__class__.__name__
+        self.db[name].insert(dict(item))
+        return item
+
+    def close_spider(self, spider):
+        # spider关闭时方法被调用
+        self.client.close()
